d33b244fd84dfc0ba11601b81d7b28a29c3419a3
Brady Hanson <brady.p.hanson@gmail.com>
2014-02-24 10:52:23 -0600

    Added Block.cs

---
+++ Yuuki2TheGame/Yuuki2TheGame/Yuuki2TheGame/Block.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Yuuki2TheGame
+{
+    class Block
+    {
+        //The item required to destroy the current block.
+        Item RequiredItem;
+        //The amount of health each block has. 
+        int MiningResistance;
+        
+        
+        public Block()
+        {
+
+        }
+
+        public void Destroy()
+        {
+            
+        }
+
+        public void BlockPlaced()
+        {
+
+        }
+    }
+}
