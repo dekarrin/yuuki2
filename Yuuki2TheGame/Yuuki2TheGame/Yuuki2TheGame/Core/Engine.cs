ba8fd8676e17360f2d6834561a4d152217026367
Rebecca Nelson <tails.fanatic@gmail.com>
2014-03-07 15:04:28 -0600

    Added methods to Engine class

--- Yuuki2TheGame/Yuuki2TheGame/Yuuki2TheGame/Core/Engine.cs
+++ Yuuki2TheGame/Yuuki2TheGame/Yuuki2TheGame/Core/Engine.cs
@@ -2,10 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace Yuuki2TheGame.Core
 {
     class Engine
     {
+        private List<GameCharacter> _characters = new List<GameCharacter>();
+
+        private List<Item> _items = new List<Item>();
+
+        private Map _map;
+
+        public Engine()
+        {
+
+        }
+
+        public void Update(GameTime gametime)
+        {
+
+        }
+
+        public Block[,] MapData
+        {
+            get
+            {
+                return null; // TODO Implement MapData property
+            }
+        }
+
+        public List<GameCharacter> Characters
+        {
+            get
+            {
+                return null; // TODO Implement Character property
+            }
+        }
+
+        public List<Item> Items
+        {
+            get
+            {
+                return null // TODO Implement Items property
+            }
+        }
     }
 }
