d41bbdd07df4db9cceff9cbc0477a762d09aafca
Rebecca Nelson <tails.fanatic@gmail.com>
2014-03-10 13:25:43 -0500

    Finished initial implementation of Camera
    Related Work Items: #7

--- Yuuki2TheGame/Yuuki2TheGame/Yuuki2TheGame/Core/Camera.cs
+++ Yuuki2TheGame/Yuuki2TheGame/Yuuki2TheGame/Core/Camera.cs
@@ -9,16 +9,38 @@ namespace Yuuki2TheGame.Core
     class Camera
     {
 
+        private ILocatable _follow;
+
         public Vector2 Location { get; private set; }
 
-        public ILocatable Following { get; set; }
+        public Vector2 Offset { get; set; }
 
-        public Camera(ILocatable gc)
-        {
-            Following = gc;
+        public ILocatable Following {
+            get
+            {
+                return _follow;
+            }
+            set
+            {
+                if (_follow != null)
+                {
+                    _follow.Moved -= HandleMovement;
+                }
+                _follow = value;
+                _follow.Moved += HandleMovement;
+            }
         }
 
+        public Camera(ILocatable gc, Vector2 offset)
+        {
+            Following = gc;
+            Offset = offset;
+        }
 
+        public void HandleMovement(object sender, MovedEventArgs e)
+        {
+            Location = e.NewLocation - Offset;
+        }
 
     }
 }
