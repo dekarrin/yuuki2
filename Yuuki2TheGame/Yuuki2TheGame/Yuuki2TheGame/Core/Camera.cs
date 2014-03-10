de5fd6401b9e161b65cc4e4d24fc9a288dc7dcb9
Rebecca Nelson <tails.fanatic@gmail.com>
2014-03-10 13:01:30 -0500

    Started creating Camera to track player movement
    Related Work Items: #7

---
+++ Yuuki2TheGame/Yuuki2TheGame/Yuuki2TheGame/Core/Camera.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Yuuki2TheGame.Core
+{
+    class Camera
+    {
+
+        public Vector2 Location { get; private set; }
+
+        public ILocatable Following { get; set; }
+
+        public Camera(ILocatable gc)
+        {
+            Following = gc;
+        }
+
+
+
+    }
+}
---
+++ Yuuki2TheGame/Yuuki2TheGame/Yuuki2TheGame/Core/ILocatable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Yuuki2TheGame.Core
+{
+    class MovedEventArgs : EventArgs
+    {
+        public Vector2 OldLocation;
+        public Vector2 NewLocation;
+    }
+
+    interface ILocatable
+    {
+        public Vector2 Location;
+
+        public delegate void MovedEventHandler(object source, MovedEventArgs e);
+
+        public event MovedEventHandler Moved;
+    }
+}
