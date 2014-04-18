e75bc21133ae9151067e7b207d3f9f1416c58b10
Brady Paul Hanson <brady.p.hanson@gmail.com>
2014-04-18 13:17:50 -0500

    Added basic inventory management to character
    Related Work Items: #6

--- Yuuki2TheGame/Yuuki2TheGame/Yuuki2TheGame/Core/PlayerCharacter.cs
+++ Yuuki2TheGame/Yuuki2TheGame/Yuuki2TheGame/Core/PlayerCharacter.cs
@@ -11,6 +11,26 @@ namespace Yuuki2TheGame.Core
         public const int WIDTH = Game1.BLOCK_WIDTH * 1;
         public const int HEIGHT = Game1.BLOCK_HEIGHT * 2;
 
+        public static Inventory inventory;
+        
+        //when no item is equipped it is represent by null
+        private Item SelectedItem = null;
+
+        //Assumes that you can only select an item in your inventory
+        public void EquipItem(Item i)
+        {
+            if (SelectedItem == null)
+            {
+                inventory.Remove(i);
+                SelectedItem = i;
+            }
+            else
+            {
+                inventory.Add(SelectedItem);
+                SelectedItem = null;
+            }
+        }
+
         public PlayerCharacter(string name, Point pixelLocation, int health, int baseAttack, int baseArmor) : base(name, pixelLocation, new Point(WIDTH, HEIGHT), health, baseAttack, baseArmor)
         {
         }
---
+++ Yuuki2TheGame/Yuuki2TheGame/Yuuki2TheGame/Data/GameObjectData.cs
@@ -0,0 +1,31 @@
+﻿using FileHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Yuuki2TheGame.Data
+{
+    [IgnoreFirst(2)]
+    [DelimitedRecord(",")]
+    public sealed class GameObjectData
+    {
+        public int ID;
+
+        public string Name;
+
+        public string Type;
+
+        public int Level;
+
+        public bool Stack;
+
+        public int MaxStack;
+
+        public bool Equipable;
+
+        public int BlockDamage;
+
+        public int Health;
+    }
+}
