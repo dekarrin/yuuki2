3083cbb8e48a19bedf281820911daf3f01aea879
Ananya Jaidev <ananya.jaidev@my.ndsu.edu>
2014-04-21 15:11:17 -0500

    Changes the color for the menu buttons in MainMenu Class

---
+++ Yuuki2TheGame/Yuuki2TheGame/Yuuki2TheGame/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Yuuki2TheGame
+{
+    class MainMenu
+    {
+        public bool selected = false;
+        public string text = "MainMenu";
+        public Rectangle area;
+
+        public MainMenu(Rectangle Area, string Text)
+        {
+            area = Area;
+            text = Text;
+
+        }
+
+        public void mouseOver(MouseState mbd)
+        {
+            if (mbd.X > area.X && mbd.X < area.X + area.Width && mbd.Y > area.Y && mbd.Y < area.Y + area.Height)
+            {
+                selected = true;
+            }
+            else
+            {
+                selected = false;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch, SpriteFont font)
+        {
+            if (selected == false)
+            {
+                spriteBatch.DrawString(font, text, new Vector2(area.X, area.Y), Color.Violet);
+
+            }
+            if (selected == true)
+            {
+                spriteBatch.DrawString(font, text, new Vector2(area.X, area.Y), Color.Aqua);
+            }
+        }
+    }
+}
