6dd3ba0b93f391a86bfc8f31ecce3c918f9c4f77
Rebecca Nelson <tails.fanatic@gmail.com>
2014-05-08 04:04:25 -0500

    Fixed ugly fade of parralax bg

--- Yuuki2TheGame/Yuuki2TheGame/Yuuki2TheGame/Core/Engine.cs
+++ Yuuki2TheGame/Yuuki2TheGame/Yuuki2TheGame/Core/Engine.cs
@@ -22,7 +22,7 @@ namespace Yuuki2TheGame.Core
     {
         public const float PHYS_WIND = 0f;
 
-        public const float PHYS_GRAVITY = 9.806f;
+        public const float PHYS_GRAVITY = 4.806f;
 
         public const float PHYS_TIMESCALE = 1.0f;
 
